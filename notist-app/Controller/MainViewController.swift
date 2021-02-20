6868957cb214fbb586cb3adee6e794a24b10fda3
mrkos-mrtinelli <m2e5090@gmail.com>
2021-02-20 12:38:06 -0800

    added navigation setup

--- notist-app/Controller/MainViewController.swift
+++ notist-app/Controller/MainViewController.swift
@@ -11,9 +11,38 @@ class MainViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Collections"
+        tableView.tableFooterView = UIView() // remove toolbar separator/border
+        
+        setupNavigationController()
     }
-
-
+    //MARK: Navigation Controller Set up
+    func setupNavigationController() {
+        // icons
+        let newFolderIcon = UIImage(systemName: "folder.badge.plus")
+        let newNoteIcon = UIImage(systemName: "square.and.pencil")
+        
+        // buttons
+        let spacer = UIBarButtonItem(systemItem: .flexibleSpace)
+        let newFolderBtn = UIBarButtonItem(image: newFolderIcon, style: .plain, target: self, action: #selector(createNewFolder))
+        let newNoteBtn = UIBarButtonItem(image: newNoteIcon, style: .plain, target: self, action: #selector(createNewNote))
+        
+        // navigationController
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.isToolbarHidden = false
+        navigationController?.toolbar.barTintColor = .white
+        navigationController?.toolbar.clipsToBounds = true
+        toolbarItems = [newFolderBtn, spacer, newNoteBtn]
+        
+    }
+    
+    //MARK: - Actions
+    @objc func createNewFolder() {
+        
+    }
+    @objc func createNewNote() {
+        
+    }
 }
 
